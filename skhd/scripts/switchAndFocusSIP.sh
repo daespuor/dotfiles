065116ffb77a6be1ecac4f56313d4352a1c4be70
Daniel <daespuor1991@gmail.com>
2022-07-30 20:13:27 -0500

    My folders

---
+++ skhd/scripts/switchAndFocusSIP.sh
@@ -0,0 +1,5 @@
+# #!/bin/dash
+
+index=$1 # input argument : 1 - 3
+
+yabai -m query --spaces | jq --argjson index "${index}" -r '(.[] | select(.index == $index).windows[0]) as $wid | if $wid then "yabai -m window --focus \"" + ($wid | tostring) + "\"" else "skhd --key \"ctrl - " + (map(select(."is-native-fullscreen" == false)) | index(map(select(.index == $index))) + 1 % 10 | tostring) + "\"" end'
